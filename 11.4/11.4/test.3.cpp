deac764594ef4eab26194c7b30c27b7f5483e0c9
477918269 <477918269@qq.com>
2018-11-04 23:02:51 +0800

    11.4

---
+++ 11.4/11.4/test.3.cpp
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int i = 0;
+//	int cd = 0;//cd²úÉúÃ¬¶Ü
+//	for(i=1; i<=4; i++)
+//	{
+//		if(i==1)
+//		{
+//			a=1;b=0;cd=1;d=1;c=1;
+//		}
+//		if(i==2)
+//		{
+//			a=0;b=0;cd=1;d=1;c=0;
+//		}
+//		if(i==3)
+//		{
+//			a=0;b=0;cd=0;d=0;c=1;
+//		}
+//		if(i==4)
+//		{
+//			a=0;b=0;cd=0;d=1;c=1;
+//		}
+//		if(a+b+c+d+cd==1 && cd==0)
+//			printf("a=%d,b=%d,c=%d,d=%d",a,b,c,d);
+//	}
+//	 
+//	return 0;
+//}
