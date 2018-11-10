7f9e0d1ab98aa7cfc97451afaecc06a64ab1bf29
477918269 <477918269@qq.com>
2018-11-10 17:07:36 +0800

    提交代码

---
+++ 11.10/11.10/test.c
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<string.h>	
+//void Chongfu(int* arr, int sz)
+//{
+////	int i = 0;
+////	int j = 0;
+////	for(i=0; i<sz-1; i++)
+////	{
+////		int count = 0;
+////		for((j=0;j<sz-1;j++) && (i!=j))
+////		{
+////			if(arr[i] == arr[j])
+////				count++;
+////		}
+////		if(count == 0)
+////		{
+////			printf("%d ",arr[i])tr
+////		}
+////	}
+//	int i = 0;
+//	while(i<sz-1)
+//	{
+//		int j = 0;
+//		int count = 0;
+//		while(j<sz)
+//		{
+//			if((arr[i] == arr[j]) && (i!=j))
+//				count++;
+//				j++;
+//		}
+//		if(count == 0)
+//			printf("%d ",arr[i]);
+//		i++;
+//	}
+//}
+//int main()
+//{
+//	int arr[] = {1,1,4,4,5,6,7,7};
+//	int sz = sizeof(arr)/sizeof(arr[0]);
+//	Chongfu(arr,sz);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 20;
+//	int j = 0;
+//	while(i != 1)
+//	{
+//		j = j+i;
+//		i = i/2;
+//		if(i%2 == 1)
+//		{
+//			++j;
+//		}
+//	}
+//	printf("%d", j);
+//	return 0;
+//}
+//#include<assert.h>
+//char* my_strcpy(char* x, const char* y)
+//{
+//
+//	//char* ret = x ;
+//	//while(*y != '\0')
+//	//{
+//	//	*x = *y;
+//	//	x++;
+//	//	y++;
+//	//}
+//	//*x = '\0';
+//	//return x;
+//	assert(x != NULL);
+//	assert(y != NULL);
+//	{
+//		char* ret = x;
+//		while(*x++ = *y++)
+//		{
+//			;
+//		}
+//		
+//		return x;
+//	}
+//
+//}
+//int main()
+//{
+//	char a[] = "you are so stupid";
+//	
+//	my_strcpy(a,"hello");
+//	printf("%s", a);
+//
+//	return 0;
+//}
+#include<string.h>
+//char* my_strcat(char* x,char* y)
+//{
+//	while(*x != 0)
+//	{
+//		x++;
+//	}
+//	while(*x++ = *y++)
+//	{
+//		;
+//	}
+//	return x;
+//}
+//int main()
+//{
+//	char a[20] = "you are so";
+//	my_strcat(a," bit");
+//	printf("%s", a);
+//
+//	return 0;
+//}
